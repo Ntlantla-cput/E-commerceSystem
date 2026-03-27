623843157af2d6adaf3700bdafcc240828ef530e
aneeqah talaaboedien <219099405@mycput.ac.za>
2026-03-27 12:56:49 +0200

    IRepository updated

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -8,6 +8,11 @@ Date March 2026
  */
 
 package za.ac.cput.repository;
+import za.ac.cput.domain.User;
 
-public interface IRepository {
+public interface IRepository <T, ID> {
+    T create (T t);
+    T read (ID id);
+    T update (T t);
+    boolean delete (ID id);
 }
